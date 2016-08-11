055cdce176e57cac2e2a93e0ac9448aa55e5a7de
brandontiqui <btiqui@gmail.com>
2016-08-10 20:32:02 -0700

    Complete Player View

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,12 +1,16 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
     this.render();
   },
 
   render: function() {
-    this.$el.append(this.collection.map(function(queueEntry) {
+    this.$el.children().detach();
+    
+    this.$el.html('<th>Queue</th>').append(this.collection.map(function(queueEntry) {
       var temp = new SongQueueEntryView({ model: queueEntry });
       return temp.render();
     }));
