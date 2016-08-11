aea29dcc990e970aba3658d7c22817cbf36b2e7f
brandontiqui <btiqui@gmail.com>
2016-08-10 18:54:04 -0700

    Complete SongModel tests. SongQueue 50% complete

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,22 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function() {
+      if (this.models.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+    this.on('ended', function(song) {
+      console.log(song);
+      this.remove(this.at(0));
+      if (this.models.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function() {
+
   }
 
 });
